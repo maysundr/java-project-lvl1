9ddeb751948db6334c91857905dfb90ada1949a2
maysundr <maysundrik@yandex.ru>
2021-12-31 17:02:52 +0300

    src/main/java/hexlet/code/games/Even.java

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -1,44 +1,19 @@
 package hexlet.code.games;
 
 
-import java.util.Scanner;
+import hexlet.code.Engine;
 
 
-public class Even {
-
-    //private static String userName = "";
-    private static final String ANSWER_YES = "yes";
-    private static final String ANSWER_NO = "no";
-    private static final int CORRECT_ANSWER = 3;
-
-    public static String greetUser() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        Scanner console = new Scanner(System.in);
-        String userName = console.nextLine();
-        System.out.println("Hello, " + userName + "!");
-        return userName;
-    }
-
-    /*public static String getNameUser() {
-        return userName;
-    }
-
-     */
-
+public class Even extends Engine {
 
     public static void evenGame() {
-        var name = Even.greetUser();
+        var name = Engine.greetUser();
         System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
         int index = 0;
         for (int i = 0; i < CORRECT_ANSWER; i++) {
-            final var factor = 100;
-            int randomNumber = (int) ((Math.random()) * factor);
+            var randomNumber = Engine.getRandomNumber();
             System.out.println("Question: " + randomNumber);
-            System.out.print("Your answer: ");
-            Scanner console = new Scanner(System.in);
-            String answerEven = console.nextLine();
-
+            var answerEven = Engine.getAnswerEven();
             if ((answerEven.equals(ANSWER_YES) && randomNumber % 2 == 0)
                     || (answerEven.equals(ANSWER_NO) && randomNumber % 2 != 0)) {
                 System.out.println("Correct!");
@@ -65,23 +40,4 @@ public class Even {
 
     }
 
-    /*public static String getNumber() {
-        final var factor = 100;
-        int randomNumber = (int) ((Math.random()) * factor);
-        System.out.println("Question: " + randomNumber);
-        System.out.print("Your answer: ");
-        Scanner console = new Scanner(System.in);
-        String answerEven = console.nextLine();
-        return answerEven;
-    }
-
-     */
-
-
-
-    /*public static void setUserName(String userName) {
-        Even.userName = userName;
-    }
-
-     */
 }
