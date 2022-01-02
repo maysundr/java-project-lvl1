bc64d1fceb70c0f99b986d0241f9fc5eb80427c0
maysundr <maysundrik@yandex.ru>
2022-01-02 23:40:29 +0300

    modified App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,13 +3,15 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
-import static hexlet.code.Engine.GAME_EXIT;
-import static hexlet.code.Engine.GAME_GREET;
-import static hexlet.code.Engine.GAME_EVEN;
 import static hexlet.code.Engine.GAME_CALC;
+import static hexlet.code.Engine.GAME_EVEN;
+import static hexlet.code.Engine.GAME_EXIT;
+import static hexlet.code.Engine.GAME_GCD;
+import static hexlet.code.Engine.GAME_GREET;
 
 public class App {
     public static void main(String[] args) {
@@ -17,6 +19,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner console = new Scanner(System.in);
@@ -34,6 +37,9 @@ public class App {
         }
         if (numberGame == GAME_CALC) {
             Calc.calcGame();
+        }
+        if (numberGame == GAME_GCD) {
+            GCD.gcdGame();
         }
     }
 }
