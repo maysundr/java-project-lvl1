0ce264bc002ed8fc92ab46991c5bacc612e7b5de
maysundr <maysundrik@yandex.ru>
2022-01-05 14:36:28 +0300

    modified App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,15 +4,17 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
-import static hexlet.code.Engine.GAME_EXIT;
-import static hexlet.code.Engine.GAME_GREET;
 import static hexlet.code.Engine.GAME_CALC;
 import static hexlet.code.Engine.GAME_EVEN;
+import static hexlet.code.Engine.GAME_EXIT;
 import static hexlet.code.Engine.GAME_GCD;
+import static hexlet.code.Engine.GAME_GREET;
+import static hexlet.code.Engine.GAME_PRIME;
 import static hexlet.code.Engine.GAME_PROGRESSION;
 
 public class App {
@@ -23,6 +25,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         Scanner console = new Scanner(System.in);
@@ -46,6 +49,9 @@ public class App {
         }
         if (numberGame == GAME_PROGRESSION) {
             Progression.progressionGame();
+        }
+        if (numberGame == GAME_PRIME) {
+            Prime.primeGame();
         }
     }
 }
