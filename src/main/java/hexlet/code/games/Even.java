2dc103c2a7f2832217192990fd99ac35b5e21e1b
maysundr <maysundrik@yandex.ru>
2022-01-06 15:02:54 +0300

    modified Even.java

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -7,37 +7,29 @@ import hexlet.code.Engine;
 public class Even extends Engine {
 
     public static void evenGame() {
+
         var name = Engine.greetUser();
+
         System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
-        int index = 0;
+
         for (int i = 0; i < CORRECT_ANSWER; i++) {
+
             var randomNumber = Engine.getRandomNumber();
+
             System.out.println("Question: " + randomNumber);
-            var answerEven = Engine.getAnswerEven();
-            if ((answerEven.equals(ANSWER_YES) && randomNumber % 2 == 0)
-                    || (answerEven.equals(ANSWER_NO) && randomNumber % 2 != 0)) {
-                System.out.println("Correct!");
-                index++;
-            } else if (answerEven.equals(ANSWER_YES)) {
-                System.out.println("'" + answerEven + "'"
-                        + " is wrong answer ;(. Correct answer was " + ANSWER_NO + ".");
-                System.out.println("Let's try again, " + name + "!");
-                System.exit(0);
-            } else if (answerEven.equals(ANSWER_NO)) {
-                System.out.println("'" + answerEven + "'"
-                        + " is wrong answer ;(. Correct answer was " + "'" + ANSWER_YES + "'.");
-                System.out.println("Let's try again, " + name + "!");
-                System.exit(0);
+            var answerUser = Engine.getAnswerUser();
+
+            if ((answerUser.equals(ANSWER_YES) && randomNumber % 2 == 0)
+                    || (answerUser.equals(ANSWER_NO) && randomNumber % 2 != 0)) {
+                getCorrectAnswer();
+            } else if (answerUser.equals(ANSWER_YES)) {
+                getWrongAnswer(answerUser, ANSWER_NO, name);
+            } else if (answerUser.equals(ANSWER_NO)) {
+                getWrongAnswer(answerUser, ANSWER_YES, name);
             } else {
-                System.out.println("'" + answerEven + "'"
-                        + " is wrong answer ;(. Correct answer was " + "'" + ANSWER_NO + "'" + ".");
-                System.exit(0);
+                getWrongAnswer(answerUser, ANSWER_NO, name);
             }
         }
-        if (index == CORRECT_ANSWER) {
-            System.out.println("Congratulations, " + name + "!");
-        }
-
+        getCongratulation(name);
     }
-
 }
